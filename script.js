5c8e988a9904e6e113a23ff0227ac2ee9aa318e6
Harif Narippatta <harifnarippatta@gmail.com>
2024-10-15 17:40:06 +0300

    complete

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function addTask() {
   const taskInput = document.getElementById("taskInput").value;
   if (taskInput.trim() !== "") {
     tasks.push(taskInput);
-    document.getElementById("taskInput").value = ""; // Clear input field
+    document.getElementById("taskInput").value = "";
     displayTasks();
   } else {
     alert("Please enter a task.");
@@ -22,7 +22,7 @@ function removeTask() {
 
 function displayTasks() {
   const taskList = document.getElementById("taskList");
-  taskList.innerHTML = ""; // Clear existing tasks
+  taskList.innerHTML = "";
   tasks.forEach((task, index) => {
     const li = document.createElement("li");
     li.textContent = `${index + 1}. ${task}`;
